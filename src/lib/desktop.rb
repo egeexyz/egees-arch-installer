9bf730cedcbe56044b3c2e43f8d5181f8ed37053
egee-irl <brian@egee.io>
2024-05-14 10:37:17 -0700

    remove the hobby stuff

--- src/lib/desktop.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'helpers'
-
-def desktop
-  pacman_install('htop lightdm lightdm-gtk-greeter mate mate-extra')
-  activate_service('lightdm')
-end
--- src/lib/partition.rb
+++
@@ -1,6 +0,0 @@
-# The \n's make the ``'s difficult to convert to stream_cmd
-def partition(disk)
-  puts `echo -e "o\nw\n" | fdisk "/dev/#{disk}"`
-  puts `echo -e "n\np\n1\n\n\nw\n" | fdisk "/dev/#{disk}"`
-  puts `mkfs.btrfs "/dev/#{disk}1"`
-end
