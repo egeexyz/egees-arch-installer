b6956c00841d0ffb6f5bb51f3e67bfe32666fa86
egee-irl <brian@egee.io>
2024-05-15 09:10:09 -0700

    cleaned up comments

--- installer.sh
+++ installer.sh
@@ -5,9 +5,9 @@ TARGET_DRIVE="${2:-sda}"
 TARGET_USER="${3:-archy}"
 
 partition() {
-  echo -e "o\nw\n" | fdisk "/dev/${TARGET_DRIVE}"           # Create new partition table
-  echo -e "n\np\n1\n\n\nw\n" | fdisk "/dev/${TARGET_DRIVE}" # Create a single partition for install
-  mkfs.ext4 "/dev/${TARGET_DRIVE}1"                         # Create a new ext4 file system
+  echo -e "o\nw\n" | fdisk "/dev/${TARGET_DRIVE}"                                                        # Create new partition table
+  echo -e "n\np\n1\n\n\nw\n" | fdisk "/dev/${TARGET_DRIVE}"                                              # Create a single partition for install
+  mkfs.ext4 "/dev/${TARGET_DRIVE}1"                                                                      # Create a new ext4 file system
 }
 
 mount_fs() {
@@ -31,17 +31,17 @@ add_user() {
   echo -e "${USER_PASSWORD}\n${USER_PASSWORD}" | passwd ${USER_NAME}
   usermod -aG adm ${USER_NAME}
   echo "${USER_NAME} ALL=(ALL:ALL) ALL" > "/etc/sudoers.d/${USER_NAME}"'
-  echo "$add_user_file" > /mnt/add_user.sh                     # Create script to add a new user
-  chmod +x /mnt/add_user.sh                                    # Make the script executable
-  arch-chroot /mnt /bin/bash -c "/add_user.sh $TARGET_USER"    # Run the script with the target user as an argument
-  arch-chroot /mnt /bin/bash -c "rm /add_user.sh"              # Delete the script
-  arch-chroot /mnt /bin/bash -c "passwd --expire $TARGET_USER" # ...And expire the user's password so they must change it upon log-in
+  echo "$add_user_file" > /mnt/add_user.sh                                                               # Create script to add a new user
+  chmod +x /mnt/add_user.sh                                                                              # Make the script executable
+  arch-chroot /mnt /bin/bash -c "/add_user.sh $TARGET_USER"                                              # Run the script with the target user as an argument
+  arch-chroot /mnt /bin/bash -c "rm /add_user.sh"                                                        # Delete the script
+  arch-chroot /mnt /bin/bash -c "passwd --expire $TARGET_USER"                                           # Expire the user's password so they must change it upon log-in
 }
 
 help() {
   echo "Available options:"
   echo "  install: Installs Arch Linux onto local system."
-  echo ""
+  echo
   echo "Example usage:"
   echo "  ./installer.sh install"
 }
